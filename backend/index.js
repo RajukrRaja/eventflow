ff1dff8932821a59b947ef7b63994bd9d0113cfd
Raju Kumar Raja <rajukumar.191813@gmail.com>
2025-04-18 19:40:13 +0530

    Configure Sequelize to connect to local MySQL database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const sequelize = require('./config/database');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Test database connection on startup
+sequelize.authenticate()
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error('Database connection error:', err));
+
 app.get('/', (req, res) => res.send('EventFlow Backend'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
